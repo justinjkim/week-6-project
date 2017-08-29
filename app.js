62e6f4f937d417d8c603aa3ffcd828b02a4c5793
Justin Kim <jjkim928@gmail.com>
2017-08-29 15:40:57 -0400

    add skeleton structure

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+
+const app = express();
+
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+app.use(express.static(__dirname + '/public'));
+app.use(body.json());
+app.use(body.urlencoded({extended: false}));
+
+app.get('/', function(req, res) {
+  res.send('Confirming');
+})
+
+
+
+app.listen(3000, function(req, res) {
+  console.log('Starting up Mystery Word app...');
+})
